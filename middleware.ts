e956adfe2e10a37cd3f57e463a8132342dfb98e4
Kunal-ahuja-88 <kahujamp4@gmail.com>
2024-07-07 19:37:10 +0530

    setting up org side bar

--- middleware.ts
+++ middleware.ts
@@ -3,9 +3,18 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 export default clerkMiddleware();
 
 export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
+
+/*
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
+
+export const config = {
   matcher: [
     "/((?!.*\\..*|_next).*)",  
     "/(api|trpc)(.*)",         
   ],
 };
-
+*/
